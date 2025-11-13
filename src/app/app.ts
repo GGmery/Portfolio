85b3e75904bbbaedeeb1d6c9a8399550c5b84ca2
GGmery <grungemxry@gmail.com>
2025-11-13 20:03:09 +0100

    creación de las cosas importantes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
+import { Home } from "./home/home";
+import { Header } from './header/header';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('portfolio');
+  ngOnInit() {
+    AOS.init()
+    window.addEventListener('load', AOS.refresh)
+  }
 }
